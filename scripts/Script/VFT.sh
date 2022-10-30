608882cbddb06b9d9d18fc842a70d3ca9e272beb
Summer Blanco (she/they) <95707842+mellamosummer@users.noreply.github.com>
2022-10-30 16:16:51 -0400

    run sleuth R script

--- scripts/Script/VFT.sh
+++ scripts/Script/VFT.sh
@@ -135,4 +135,4 @@ multiqc $OUTDIR/FastQC/pretrim/*.zip -o $OUTDIR/FastQC/pretrim/
 # mkdir $OUTDIR/sleuth
 
 source activate R
-R --no-save < /home/srb67793/2022VenusFlyTrap/scripts/sleuth.R
+R --no-save < /home/srb67793/2022VenusFlyTrap/scripts/Script/sleuth.R
