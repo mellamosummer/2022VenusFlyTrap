d293cf2f6356ad114e8ba261ff54940efc85a77e
Summer Blanco (she/they) <95707842+mellamosummer@users.noreply.github.com>
2022-10-30 16:36:19 -0400

    removed duplicate script

---
+++ scripts/Script/sleuth_timeseries.R
@@ -0,0 +1,45 @@
+suppressMessages({
+ library("sleuth")
+})
+
+suppressMessages({
+ library("splines")
+})
+
+#set input and output dirs
+datapath <- "/scratch/srb67793/2022VenusFlyTrap/kallisto/quant/"
+resultdir <- "/scratch/srb67793/2022VenusFlyTrap/sleuth"
+
+setwd(resultdir
+
+s2c <- read.table("VFT_samples_condition_time_path.csv.tsv", header = TRUE, stringsAsFactors = FALSE)
+s2c <- dplyr::mutate(s2c, path = paths)
+s2c[] <- lapply(s2c, as.character)
+s2c
+
+
+day <- s2c$time
+full_design <- model.matrix(formula(~ ns(time, df = 4)))
+full_design
+
+so <- sleuth_prep(s2c, full_model = full_design, target_mapping = t2g)
+so <- sleuth_fit(so)
+so <- sleuth_fit(so, formula = ~ 1, fit_name = "reduced")
+so <- sleuth_lrt(so, "reduced", "full")
+
+plot_qq(so, test = 'reduced:full', test_type = 'lrt', sig_level = 0.05)
+
+lrt_results <- sleuth_results(so, 'reduced:full', test_type = 'lrt')
+table(lrt_results[,"qval"] < 0.05)
+
+lrt_results %>% head(n = 20) %>% dplyr::select(target_id, qval)
+
+plot_transcript_heatmap(so, head(lrt_results, n = 20)$target_id, 'est_counts')
+
+# tmp <- so$obs_raw %>% dplyr::filter(target_id == 'INPUT TARGET ID')
+# tmp <- dplyr::full_join(so$sample_to_covariates, tmp, by = 'sample')
+# tmp
+# tmp <- transform(tmp, time = as.numeric(time))
+# ggplot(tmp, aes(x=time, y=est_counts))
+#   + geom_point(shape=1)
+#   + geom_smooth(method = loess)
