4288df6b51874ab262b40777ddba103908415996
Summer Blanco (she/they) <95707842+mellamosummer@users.noreply.github.com>
2022-11-03 14:41:43 -0400

    updating edgeR & masigpro

--- Cleaned_Workflow/Scripts/MaSigPro.R
+++ Cleaned_Workflow/Scripts/MaSigPro.R
@@ -1,14 +1,14 @@
-suppressMessages({library("maSigPro")
+suppressMessages({
 library("tximport")
 library("readr")
 library("tidyr")
 library("dplyr")})
 
 suppressMessages({
-if (!require("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-
-BiocManager::install("edgeR")})
+install.packages("edgeR")
+install.packages("MaSigPro")
+library("edgeR")
+library("MaSigPro")})
 
 #set input and output dirs
 datapath <- "/scratch/srb67793/2022VenusFlyTrap/kallisto/quant/"  # you need to modify this line to match the path made by your BASH script
@@ -155,27 +155,46 @@ edesign.VFT <- as.data.frame(edesign.VFT)
 
 #matches column names of counts matrix to edesign matrix
 colnames(normdm) <- rownames(edesign.VFT)
-str(normdm)
-colnames(normdm)
-rownames(edesign.VFT)
-colnames(edesign.VFT)
-rownames(normdm)
 
+#make exp. design for masigpro
 design <- make.design.matrix(edesign.VFT, degree = 7)
 design$groups.vector
 
-##using the negative binomial options in masigpro, calculate polynomial regressions for each gene
-NBp<-p.vector(normdm, design, counts=TRUE) #please choose dis. family with care. default for counts is neg. binomial
+#using the negative binomial options in masigpro, calculate polynomial regressions for each gene
+NBp<-p.vector(normdm, design, counts=TRUE) #
 NBt<-T.fit(NBp)
 
-sigs <- get.siggenes(NBt, rsq = 0.6, vars = "groups")
+#get list of significant genes
+sigs <- get.siggenes(NBt)
+
+#visualize clusters
+k8vis<-see.genes(sigs$sig.genes, k = 8)
+k6vis<-see.genes(sigs$sig.genes, k = 6)
 
 pdf(file="MaSigProPreySeriesCluster8.pdf")
-see.genes(sigs$sig.genes, show.fit = T, dis =design$dis,
-cluster.method="hclust" ,cluster.data = 1, k = 8)
+k8vis
 dev.off()
 
-pdf(file="MaSigProPreySeriesCluster6.pdf")
-see.genes(sigs$sig.genes, show.fit = T, dis =design$dis,
-cluster.method="hclust" ,cluster.data = 1, k = 6)
+pdf(file="MaSigProPreySeriesCluster8.pdf")
+k6vis
 dev.off()
+
+#get genes in each cluster
+cluster1 <- names(which(k8vis$cut==1))
+cluster2 <- names(which(k8vis$cut==2))
+cluster3 <- names(which(k8vis$cut==3))
+cluster4 <- names(which(k8vis$cut==4))
+cluster5 <- names(which(k8vis$cut==5))
+cluster6 <- names(which(k8vis$cut==6))
+cluster7 <- names(which(k8vis$cut==7))
+cluster8 <- names(which(k8vis$cut==8))
+
+#make cluster csv's
+write.csv(x = cluster1, file = "MaSigProCluster1.csv")
+write.csv(x = cluster2, file = "MaSigProCluster2.csv")
+write.csv(x = cluster3, file = "MaSigProCluster3.csv")
+write.csv(x = cluster4, file = "MaSigProCluster4.csv")
+write.csv(x = cluster5, file = "MaSigProCluster5.csv")
+write.csv(x = cluster6, file = "MaSigProCluster6.csv")
+write.csv(x = cluster7, file = "MaSigProCluster7.csv")
+write.csv(x = cluster8, file = "MaSigProCluster8.csv")
